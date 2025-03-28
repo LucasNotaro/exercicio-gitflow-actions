38dd89fc81659e75f5b9179c160c3d28c0b569ce
LucasNotaro <lucas.sn@live.com>
2025-03-28 16:06:20 -0300

    fix(ci): novo teste de versionamento

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ console.log("Alteração feita na branch develop");
 console.log("Erro corrigido na produção!");
 console.log("Teste CI");
 console.log("Teste CI agora com o yml na branch correta");
-console.log("Nova funcionalidade para versionamento automático,com nova correção de token");
+console.log("Funcionalidade para versionamento automático,com nova correção de token");
 
 
 module.exports = app;
